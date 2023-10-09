483da49d73dc1c0719f084ecca3d304b8d7ef9e6
Kileia <kileia17@gmail.com>
2023-10-09 00:28:15 -0700

    update

---
+++ quiz/answers.js
@@ -0,0 +1,82 @@
+//Kileia Riley - Lab 2 part 2
+//Quiz
+function answer1(a1)
+{
+    if (a1 === "Bella") {
+        return "right";
+    } else {
+        return "wrong";
+    }
+}
+function answer2(a2)
+{
+    if (a2 === "Edward") {
+        return "right";
+    } else {
+        return "wrong";
+    }
+}
+function answer3(a3)
+{
+    if (a3 === "Jacob") {
+        return "right";
+    } else {
+        return "wrong";
+    }
+}
+function answer4(a4)
+{
+    if (a4 === "Carlisle") {
+        return "right";
+    } else {
+        return "wrong";
+    }
+}
+function answer5(a5)
+{
+    if (a5 === "Charlie") {
+        return "right";
+    } else {
+        return "wrong";
+    }
+}
+function answer6(a6)
+{
+    if (a6 === "Alice") {
+        return "right";
+    } else {
+        return "wrong";
+    }
+}
+function answer7(a7)
+{
+    if (a7 === "James") {
+        return "right";
+    } else {
+        return "wrong";
+    }
+}
+function answer8(a8)
+{
+    if (a8 === "Aro") {
+        return "right";
+    } else {
+        return "wrong";
+    }
+}
+function answer9(a9)
+{
+    if (a9 === "Renesmee") {
+        return "right";
+    } else {
+        return "wrong";
+    }
+}
+function answer10(a10)
+{
+    if (a10 === "Jasper") {
+        return "right";
+    } else {
+        return "wrong";
+    }
+}
